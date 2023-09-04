dcddff9609406ca2b4cdd5ce2c96cdae688ea3a4
Grindelwaldoff <vsevolodrybnik@gmail.com>
2023-09-04 17:07:24 +0300

    review 2

--- src/main.py
+++ src/main.py
@@ -9,13 +9,12 @@ from tqdm import tqdm
 from constants import BASE_DIR, MAIN_DOC_URL, PEP_DOC_URL, EXPECTED_STATUS
 from configs import configure_argument_parser, configure_logging
 from outputs import control_output
+from exceptions import EmptyTagList
 from utils import find_tag, get_response
 
 
-def whats_new(session):
-    # Вместо константы WHATS_NEW_URL, используйте переменную whats_new_url.
-    whats_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
-    response = get_response(session, whats_new_url)
+def make_soup(session, url, features='lxml'):
+    response = get_response(session, url)
     if response is None:
         logging.exception(
             f'Возникла ошибка при загрузке страницы {url}',
@@ -24,15 +23,25 @@ def whats_new(session):
         return
     # Создание "супа".
     soup = BeautifulSoup(response.text, features='lxml')
+    return soup
+
+
+def whats_new(session):
+    # Вместо константы WHATS_NEW_URL, используйте переменную whats_new_url.
+    whats_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
+    print(whats_new_url)
+    soup = make_soup(session, whats_new_url)
 
     # Шаг 1-й: поиск в "супе" тега section с нужным id. Парсеру нужен только
     # первый элемент, поэтому используется метод find().
-    main_div = find_tag(soup, 'section', attrs={'id': 'what-s-new-in-python'})
-
     # Шаг 2-й: поиск внутри main_div следующего тега div с классом
     # toctree-wrapper.
     # Здесь тоже нужен только первый элемент, используется метод find().
-    div_with_ul = find_tag(main_div, 'div', attrs={'class': 'toctree-wrapper'})
+    div_with_ul = find_tag(
+        soup,
+        'div',
+        attrs={'class': 'toctree-wrapper'}
+    )
 
     # Шаг 3-й: поиск внутри div_with_ul всех элементов списка li с классом
     # toctree-l1.
@@ -62,20 +71,13 @@ def whats_new(session):
 
 
 def latest_versions(session):
-    response = get_response(session, MAIN_DOC_URL)
-    if response is None:
-        return
-    soup = BeautifulSoup(response.text, features='lxml')
-    sidebar = find_tag(soup, 'div', class_='sphinxsidebarwrapper')
-    ul_tags = sidebar.find_all('ul')
-    for ul in ul_tags:
-        if 'All versions' in ul.text:
-            a_tags = ul.find_all('a')
-            break
-    else:
-        raise Exception('Ничего не нашлось')
+    soup = make_soup(session, MAIN_DOC_URL)
+    sidebar = find_tag(soup, 'div', attrs={'class': 'sphinxsidebarwrapper'})
+    a_tags = sidebar.find_all('a')
+    if a_tags == []:
+        raise EmptyTagList('Ничего не нашлось')
     pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
-    results = []
+    results = [('Link', 'ver.', 'State')]
     for version in a_tags:
         link = urljoin(MAIN_DOC_URL, version['href'])
         text_match = re.search(pattern, version.text)
@@ -91,11 +93,7 @@ def download(session):
     # Вместо константы DOWNLOADS_URL, используйте переменную downloads_url.
     downloads_url = urljoin(MAIN_DOC_URL, 'download.html')
 
-    response = get_response(session, downloads_url)
-    if response is None:
-        return
-
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = make_soup(session, downloads_url)
 
     main_tag = find_tag(soup, 'div', {'role': 'main'})
     table_tag = find_tag(main_tag, 'table', {'class': 'docutils'})
@@ -123,20 +121,16 @@ def pep(session):
         ('Status', 'Amount')
     ]
     statuses = {
-        'A': 0,
-        'D': 0,
-        'F': 0,
-        'P': 0,
-        'R': 0,
-        'S': 0,
-        'W': 0,
-        '': 0,
+        'Active': 0,
+        'Deffered': 0,
+        'Final': 0,
+        'Provisional': 0,
+        'Rejected': 0,
+        'Superseded': 0,
+        'Withdrawn': 0,
+        '*Draft/Active': 0,
     }
-    response = get_response(session, PEP_DOC_URL)
-    if response is None:
-        return
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = make_soup(session, PEP_DOC_URL)
     index_by_categroy_section = find_tag(
         soup,
         'section',
@@ -160,11 +154,7 @@ def pep(session):
             find_tag(pep_rows[pep_data_index+1], 'a')['href']
         )
         # узнаем статус в карточке
-        response = get_response(session, pep_link)
-        if response is None:
-            return
-        response.encoding = 'utf-8'
-        soup = BeautifulSoup(response.text, 'lxml')
+        soup = make_soup(session, pep_link)
         status_sibling_finder = find_tag(soup, 'dl')
         for tag in status_sibling_finder:
             if 'Status' in tag:
@@ -178,7 +168,11 @@ def pep(session):
                 EXPECTED_STATUS[table_status[1:]]
             ))
         else:
-            statuses[table_status[1:]] += 1
+            for k in statuses.keys():
+                if k.startswith(table_status[1:]):
+                    statuses[k] += 1
+                elif table_status[1:] == '':
+                    statuses['*Draft/Active'] += 1
 
     print('Несовпадающие статусы:')
     for pep in mismatched_peps:
